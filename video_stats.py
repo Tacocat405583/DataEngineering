847ed51f8c865db6a0143e47d325fa574219ed9d
unknown <nicolashernan2029@gmail.com>
2025-09-16 13:24:57 -0700

    added get_video_ids func

--- video_stats.py
+++ video_stats.py
@@ -9,6 +9,8 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "3blue1brown"
+maxResults = 50
+
 
 def get_playlist_id():
     
@@ -37,8 +39,55 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+
+
+
+
+def get_video_ids(playlistId):
+    
+    video_ids = []
+    
+    pageToken = None
+    
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    
+    try:
+        
+        while True:
+            
+            url = base_url
+            
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+                
+            response = requests.get(url)
+        
+            response.raise_for_status()
+
+            data = response.json()
+            
+            
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+                
+            pageToken = data.get('nextPageToken')
+            
+            #no more videos
+            if not pageToken:
+                break
+            
+        
+        return video_ids
+                
+            
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+    
     
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    print(get_video_ids(playlistId))
 
         
